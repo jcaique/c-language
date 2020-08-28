150b85de6e26ef6ba87efb6d31cbeafd73cc5a9f
José Caíque <Joseph2@jcaique.caique>
2020-08-28 13:51:37 -0300

    The last for today

---
+++ 4-sum-2-numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+    This program sum two number until 
+    the user stop
+*/
+
+int main()
+{
+    float number1, number2, result;
+    int stop = 1;
+
+    while (stop == 1)
+    {
+        printf("Type the first number: \n");
+        scanf("%f", &number1);
+
+        printf("Type the second number: \n");
+        scanf("%f", &number2);
+
+        result = number1 + number2;
+
+        system("cls");
+
+        printf("Result of the sum by %d + %d: %.2f \n", number1, number2, result);
+
+        printf("Type 1 to continue or any for stop.\n");
+        scanf("%d", &stop);
+    }
+
+    printf("That's cool!");
+}

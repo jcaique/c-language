437b03a2c47dc93d1d1e823910e130b150e3aa5b
José Caíque L. Silva <70341631+jcaique@users.noreply.github.com>
2021-03-12 09:39:59 -0300

    simple edit
    requesting to user input the quantity in the array

--- 6-array-number.c
+++ 6-array-number.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#define size 10
+//#define size 10
 
 int main()
 {
     //Declaring an array with 10 positions to int numbers
-    int array[size];
+    int array[size], arrLen;
     int i; //the counter to loop for
 
+    printf("Insert a qunatity value for the int array: ");
+    scanf("%d", &arrLen);
+    
     //Getting values to each array's positions
-    for (i = 0; i < size; i++)
+    for (i = 0; i < arrLen; i++)
     {
         printf("Array posisiton %i insert a int value: ", i);
         scanf("%d", &array[i]);
     }
 
     printf("Values storeds in the array:\n");
-    for (i = 0; i < size; i++)
+    for (i = 0; i < arrLen; i++)
     {
         printf("%d", array[i]);
     }
